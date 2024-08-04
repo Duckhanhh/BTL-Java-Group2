a33bbc12cbf353b670c35078a3b923c987eb52a8
duc181004 <tranxuanduc181004@gmail.com>
2024-08-04 22:52:39 +0700

    update DiaChi

--- BTL-Java-Group2/src/Main.java
+++
@@ -1,7 +0,0 @@
-import Controller.MotelController;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(MotelController.getInstance().addMotel("HN"));
-    }
-}
