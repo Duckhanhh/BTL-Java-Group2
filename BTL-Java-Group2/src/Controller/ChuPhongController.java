e9fd29fad7d0ec65a9cf17a773792a7da552f644
duc181004 <tranxuanduc181004@gmail.com>
2024-08-16 22:50:10 +0700

    update sua tro, xoa khach khoi tro, tim tro not

--- BTL-Java-Group2/src/Controller/ChuPhongController.java
+++ BTL-Java-Group2/src/Controller/ChuPhongController.java
@@ -1,7 +1,9 @@
 package Controller;
 
 
+import Model.ChuPhong;
 import Model.DiaChi;
+import Model.KhachHang;
 import Model.Phong;
 import Repo.DAO.PhongDAO;
 
@@ -11,5 +13,13 @@ public class ChuPhongController {
 
     public List<Phong> timKiem(String Tinh, String Huyen, String Xa, String TenDuong, String soNha, Integer timKiemGiaTu, Integer timKiemGiaDen) {
         return PhongDAO.getInstance().findPhong(Tinh, Huyen, Xa, TenDuong, soNha, timKiemGiaTu, timKiemGiaDen);
+    }
+
+    public void xoaKhachKhoiTro(Phong obj) {
+        PhongDAO.getInstance().delete(obj);
+    }
+
+    public void suaPhong(Long id, DiaChi diaChi, Double gia, String moTa, Double dienTich, String hinhAnh, ChuPhong chu, KhachHang khach) {
+        PhongDAO.getInstance().updatePhong(id, diaChi, gia, moTa, dienTich, hinhAnh, chu, khach);
     }
 }
